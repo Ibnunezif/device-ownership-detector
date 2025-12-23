923fac6d5dbcb34ca48320ea1604968303ce7e53
Abdulbasit Nezif Abasambi <142674760+Ibnunezif@users.noreply.github.com>
2025-12-23 15:59:58 +0300

    device type

---
+++ backend/Models/deviceTypeModel.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose"
+
+const Schema = mongoose.Schema
+
+const deviceTypeSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+  },
+  created_by: {
+    type: String,
+    required: true,
+    ref: 'User',
+  },
+  
+}, { timestamps: true })
+
+export default mongoose.model('DeviceType', deviceTypeSchema);
---
+++ backend/controllers/deviceTypeController.js
@@ -0,0 +1,10 @@
+import deviceType from "../Models/deviceTypeModel";
+//create device types
+
+//get all device types
+
+//get single device type
+
+//update device Type
+
+//delate device Type
