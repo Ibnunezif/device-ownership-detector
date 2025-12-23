bad3bab9ca9f928cfd12e253064807de8f94f091
Salah <mosalahkd028@gmail.com>
2025-12-22 23:40:46 -0700

    Add gate schema

---
+++ backend/Models/gate.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const gateSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    location: { type: String, required: true },
+    created_by_user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Gate = mongoose.model("Gate", gateSchema);
+
+export default Gate;
