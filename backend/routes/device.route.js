388344905b2065bcc90b2e2bd2117045dd522803
Salah <mosalahkd028@gmail.com>
2025-12-24 01:02:05 -0700

    Add flag routes

--- backend/routes/device.route.js
+++ backend/routes/device.route.js
@@ -2,6 +2,8 @@ import express from "express";
 import {
   registerDevice,
   approveDevice,
+  flagDeviceStatus,
+  getFlaggedDevices,
 } from "../controllers/device.controller.js";
 import mockStudentAuth from "../middleware/mockRequireAuth.js";
 import upload from "../middleware/multer.js";
@@ -17,5 +19,11 @@ deviceRouter.post(
 );
 
 deviceRouter.post("/approve/:deviceId", mockRequireAuth, approveDevice);
+
+// Flag device as stolen (security_chief only)
+deviceRouter.post("/flag/:id", mockRequireAuth, flagDeviceStatus);
+
+// List flagged devices (security_staff & security_chief)
+deviceRouter.get("/flagged", mockRequireAuth, getFlaggedDevices);
 
 export default deviceRouter;
