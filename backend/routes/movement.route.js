df53f2c695a3369f93453d02586715d914716380
Salah <mosalahkd028@gmail.com>
2025-12-23 23:38:39 -0700

    Add scan route

--- backend/routes/movement.route.js
+++ backend/routes/movement.route.js
@@ -3,12 +3,14 @@ import mockRequireAuth from "../middleware/mockRequireAuth.js";
 import {
   createMovement,
   getMovements,
+  scanDevice,
 } from "../controllers/movement.controller.js";
 
 const movementRouter = express.Router();
 
 movementRouter.post("/create", mockRequireAuth, createMovement);
 movementRouter.get("/all", mockRequireAuth, getMovements);
+movementRouter.post("/scan", mockRequireAuth, scanDevice);
 // Audit logging only: no update or delete endpoints
 
 export default movementRouter;
